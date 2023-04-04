84c1944d8afc02174ead150b76f4aa1fba0540be
RyanKoech <sirryankoech@gmail.com>
2023-04-04 13:59:30 +0300

    enhancement-1 :: defined api service interface

--- feature_home/src/main/java/com/ryankoech/hogwarts/feature_home/data/data_source/remote/CharactersServiceApi.kt
+++ feature_home/src/main/java/com/ryankoech/hogwarts/feature_home/data/data_source/remote/CharactersServiceApi.kt
@@ -1,7 +1,12 @@
 package com.ryankoech.hogwarts.feature_home.data.data_source.remote
 
+import com.ryankoech.hogwarts.feature_home.data.dto.character_dto.CharacterDto
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface CharactersServiceApi {
 
-
+    @GET("characters")
+    suspend fun getCharacters() : Response<CharacterDto>
 
 }
