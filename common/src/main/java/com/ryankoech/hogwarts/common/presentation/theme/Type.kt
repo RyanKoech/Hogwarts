e1fb6b4ba3600244a58a5c100bc3f188798f31eb
RyanKoech <sirryankoech@gmail.com>
2023-04-04 18:51:46 +0300

    enhancement-1 :: refactor theme file location

--- common/src/main/java/com/ryankoech/hogwarts/common/presentation/theme/Type.kt
+++ common/src/main/java/com/ryankoech/hogwarts/common/presentation/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.ryankoech.hogwarts.ui.theme
+package com.ryankoech.hogwarts.common.presentation.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
