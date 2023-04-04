b3fe7a8c357c46db5842b70b52eb2dc41e9c500b
RyanKoech <sirryankoech@gmail.com>
2023-04-04 13:16:41 +0300

    enhancement-1 :: implement retrofit di provider

---
+++ common/src/main/java/com/ryankoech/common/core/di/ApiServiceModule.kt
@@ -0,0 +1,46 @@
+package com.ryankoech.common.core.di
+
+import com.ryankoech.common.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+private const val BASE_URL = "https://hp-api.onrender.com/api/"
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApiServiceModule {
+
+    @Singleton
+    @Provides
+    fun provideLoggingInterceptor() : OkHttpClient {
+
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY
+        else HttpLoggingInterceptor.Level.NONE
+
+        return OkHttpClient.Builder()
+            .readTimeout(120, TimeUnit.SECONDS)
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+
+}
