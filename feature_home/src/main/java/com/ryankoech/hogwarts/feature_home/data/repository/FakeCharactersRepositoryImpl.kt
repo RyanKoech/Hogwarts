47340f0f36175ff4a75f7258df2e272c5be63572
RyanKoech <sirryankoech@gmail.com>
2023-04-05 20:04:55 +0300

    testing-5 :: add more fake data

--- feature_home/src/main/java/com/ryankoech/hogwarts/feature_home/data/repository/FakeCharactersRepositoryImpl.kt
+++ feature_home/src/main/java/com/ryankoech/hogwarts/feature_home/data/repository/FakeCharactersRepositoryImpl.kt
@@ -13,32 +13,86 @@ class FakeCharactersRepositoryImpl @Inject constructor() : CharactersRepository 
 
         fun getFakeCharacterDto() : CharacterDto {
             val  characterDto = CharacterDto()
-            characterDto.add(
-                CharacterDtoItem(
-                    id ="9e3f7ce4-b9a7-4244-b709-dae5c1f1d4a8",
-                    name ="Harry Potter",
-                    alternate_names = listOf("The Boy Who Lived","The Chosen One"),
-                    species ="human",
-                    gender ="male",
-                    house ="Gryffindor",
-                    dateOfBirth ="31-07-1980",
-                    yearOfBirth =1980,
-                    wizard =true,
-                    ancestry ="half-blood",
-                    eyeColour ="green",
-                    hairColour ="black",
-                    wand = Wand(
-                        wood ="holly",
-                        core ="phoenix feather",
-                        length =11.0,
+            characterDto.addAll(
+                listOf(
+                    CharacterDtoItem(
+                        id ="9e3f7ce4-b9a7-4244-b709-dae5c1f1d4a8",
+                        name ="Harry Potter",
+                        alternate_names = listOf("The Boy Who Lived","The Chosen One"),
+                        species ="human",
+                        gender ="male",
+                        house ="Gryffindor",
+                        dateOfBirth ="31-07-1980",
+                        yearOfBirth =1980,
+                        wizard =true,
+                        ancestry ="half-blood",
+                        eyeColour ="green",
+                        hairColour ="black",
+                        wand = Wand(
+                            wood ="holly",
+                            core ="phoenix feather",
+                            length =11.0,
+                        ),
+                        patronus ="stag",
+                        hogwartsStudent =true,
+                        hogwartsStaff =false,
+                        actor ="Daniel Radcliffe",
+                        alternate_actors = listOf(),
+                        alive =true,
+                        image ="https://ik.imagekit.io/hpapi/harry.jpg"
                     ),
-                    patronus ="stag",
-                    hogwartsStudent =true,
-                    hogwartsStaff =false,
-                    actor ="Daniel Radcliffe",
-                    alternate_actors = listOf(),
-                    alive =true,
-                    image ="https://ik.imagekit.io/hpapi/harry.jpg"
+                    CharacterDtoItem(
+                        id ="9e3f7ce4-b9a7-4244-b709",
+                        name ="Garry Potter",
+                        alternate_names = listOf("The Boy Who Lived","The Chosen One"),
+                        species ="human",
+                        gender ="male",
+                        house ="Slytherin",
+                        dateOfBirth ="31-07-1980",
+                        yearOfBirth =1980,
+                        wizard =true,
+                        ancestry ="half-blood",
+                        eyeColour ="green",
+                        hairColour ="black",
+                        wand = Wand(
+                            wood ="holly",
+                            core ="phoenix feather",
+                            length =11.0,
+                        ),
+                        patronus ="stag",
+                        hogwartsStudent =true,
+                        hogwartsStaff =false,
+                        actor ="Daniel Radcliffe",
+                        alternate_actors = listOf(),
+                        alive =true,
+                        image ="https://ik.imagekit.io/hpapi/harry.jpg"
+                    ),
+                    CharacterDtoItem(
+                        id ="9e3f7ce4-b709-dae5c1f1d4a8",
+                        name ="Marry Potter",
+                        alternate_names = listOf("The Boy Who Lived","The Chosen One"),
+                        species ="human",
+                        gender ="male",
+                        house ="Ravenclaw",
+                        dateOfBirth ="31-07-1980",
+                        yearOfBirth =1980,
+                        wizard =true,
+                        ancestry ="half-blood",
+                        eyeColour ="green",
+                        hairColour ="black",
+                        wand = Wand(
+                            wood ="holly",
+                            core ="phoenix feather",
+                            length =11.0,
+                        ),
+                        patronus ="stag",
+                        hogwartsStudent =true,
+                        hogwartsStaff =false,
+                        actor ="Daniel Radcliffe",
+                        alternate_actors = listOf(),
+                        alive =true,
+                        image ="https://ik.imagekit.io/hpapi/harry.jpg"
+                    )
                 )
             )
             return characterDto
