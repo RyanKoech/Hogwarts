3633dfa7f244abdca0db2c010460fbd9b8b13180
RyanKoech <sirryankoech@gmail.com>
2023-04-05 11:40:34 +0300

    enhancement-2 :: add sreens sealed class

---
+++ app/src/main/java/com/ryankoech/hogwarts/presentation/navigation/Screens.kt
@@ -0,0 +1,19 @@
+package com.ryankoech.hogwarts.presentation.navigation
+
+import androidx.annotation.StringRes
+import com.ryankoech.hogwarts.R
+
+
+sealed class Screens(
+    val route : String,
+    @StringRes val titleResId : Int,
+) {
+    object Home : Screens(
+        route = "home",
+        titleResId = R.string.app_name,
+    )
+    object Character : Screens(
+        route = "character",
+        titleResId = R.string.app_name,
+    )
+}
