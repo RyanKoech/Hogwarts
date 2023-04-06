49a38a5aca18290bc1cc19f6b763ce9bee07c33b
RyanKoech <sirryankoech@gmail.com>
2023-04-06 12:23:02 +0300

    testing-5 :: implement homescreen tests

---
+++ feature_home/src/androidTest/java/com/ryankoech/hogwarts/feature_home/presentation/HomeScreenTest.kt
@@ -0,0 +1,136 @@
+package com.ryankoech.hogwarts.feature_home.presentation
+
+import androidx.activity.ComponentActivity
+import androidx.compose.material.Surface
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import com.google.common.truth.Truth
+import com.ryankoech.hogwarts.common.core.util.Resource
+import com.ryankoech.hogwarts.common.presentation.components.TEST_TAG_ERROR_SCREEN
+import com.ryankoech.hogwarts.common.presentation.components.TEST_TAG_LOADING_SCREEN
+import com.ryankoech.hogwarts.common.presentation.theme.HogwartsTheme
+import com.ryankoech.hogwarts.feature_home.data.dto.character_dto.CharacterDto
+import com.ryankoech.hogwarts.feature_home.data.repository.FakeCharactersRepositoryImpl
+import com.ryankoech.hogwarts.feature_home.domain.usecase.GetCharactersUseCase
+import com.ryankoech.hogwarts.feature_home.presentation.viewmodel.HomeViewModel
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit4.MockKRule
+import io.mockk.mockk
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.test.*
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class HomeScreenTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @get:Rule
+    val mockkRule = MockKRule(this)
+
+    @MockK
+    private lateinit var getCharactersUseCase: GetCharactersUseCase
+
+    private lateinit var viewModel : HomeViewModel
+
+    private lateinit var charactersFlows : Flow<Resource<CharacterDto>>
+
+    private lateinit var dispatcher: CoroutineDispatcher
+
+    @Before
+    fun setUp() {
+        dispatcher = Dispatchers.Unconfined
+        Dispatchers.setMain(dispatcher)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun beginFetchCoins_ShowLoadingScreen() {
+        charactersFlows = flow {
+            emit(Resource.Loading())
+        }
+
+        coEvery { getCharactersUseCase(any(), any()) } returns charactersFlows
+
+        viewModel = HomeViewModel(getCharactersUseCase, dispatcher)
+
+        composeTestRule.setContent {
+            HogwartsTheme {
+                Surface {
+                    HomeScreen(
+                        navigateToCharacterScreen = {},
+                        viewModel = viewModel,
+                    )
+                }
+            }
+        }
+
+        composeTestRule.onNodeWithTag(TEST_TAG_LOADING_SCREEN).assertIsDisplayed()
+
+    }
+
+    @Test
+    fun fetchCoins_ShowSuccessScreen() {
+        charactersFlows = flow {
+            emit(Resource.Success(FakeCharactersRepositoryImpl.getFakeCharacterDto()))
+        }
+
+        coEvery { getCharactersUseCase(any(), any()) } returns charactersFlows
+
+        viewModel = HomeViewModel(getCharactersUseCase, dispatcher)
+
+
+        composeTestRule.setContent {
+            HogwartsTheme {
+                Surface {
+                    HomeScreen(
+                        navigateToCharacterScreen = {},
+                        viewModel = viewModel,
+                    )
+                }
+            }
+        }
+
+        composeTestRule.onNodeWithTag(TEST_TAG_HOME_SCREEN_SUCCESS).assertIsDisplayed()
+
+    }
+
+    @Test
+    fun fetchCoinsWithUseCaseError_ShowErrorScreen() {
+        charactersFlows = flow {
+            emit(Resource.Error(""))
+        }
+
+        coEvery { getCharactersUseCase(any(), any()) } returns charactersFlows
+
+        viewModel = HomeViewModel(getCharactersUseCase, dispatcher)
+
+        composeTestRule.setContent {
+            HogwartsTheme {
+                Surface {
+                    HomeScreen(
+                        navigateToCharacterScreen = {},
+                        viewModel = viewModel,
+                    )
+                }
+            }
+        }
+
+        composeTestRule.onNodeWithTag(TEST_TAG_ERROR_SCREEN).assertIsDisplayed()
+
+    }
+}
