096e6f011c431dd546762a5bf1304e41b6a6feb4
RyanKoech <sirryankoech@gmail.com>
2023-04-06 15:24:16 +0300

    testing-5 :: add jetbrains kotlin bom dependencies

---
+++ feature_character/src/androidTest/java/com/ryankoech/hogwarts/feature_character/presentation/CharacterScreenTest.kt
@@ -0,0 +1,17 @@
+package com.ryankoech.hogwarts.feature_character.presentation
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+
+class CharacterScreenTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
