ff12aa9c62fb445755cff142bcb80bcdbaaac764
RyanKoech <sirryankoech@gmail.com>
2023-04-07 23:19:47 +0300

    bug-14 :: add annotations to prevent null data from obfusaction

--- feature_home/src/main/java/com/ryankoech/hogwarts/feature_home/data/dto/character_dto/CharacterDtoItem.kt
+++ feature_home/src/main/java/com/ryankoech/hogwarts/feature_home/data/dto/character_dto/CharacterDtoItem.kt
@@ -1,24 +1,27 @@
 package com.ryankoech.hogwarts.feature_home.data.dto.character_dto
 
+import androidx.annotation.Keep
+import com.google.gson.annotations.SerializedName
+
 data class CharacterDtoItem(
-    val actor: String,
-    val alive: Boolean,
-    val alternate_actors: List<String>,
-    val alternate_names: List<String>,
-    val ancestry: String,
-    val dateOfBirth: String?,
-    val eyeColour: String,
-    val gender: String,
-    val hairColour: String,
-    val hogwartsStaff: Boolean,
-    val hogwartsStudent: Boolean,
-    val house: String,
-    val id: String,
-    val image: String,
-    val name: String,
-    val patronus: String,
-    val species: String,
-    val wand: Wand,
-    val wizard: Boolean,
-    val yearOfBirth: Int?
+    @Keep @SerializedName("actor") val actor: String?,
+    @Keep @SerializedName("alive") val alive: Boolean?,
+    @Keep @SerializedName("alternate_actors") val alternate_actors: List<String?>?,
+    @Keep @SerializedName("alternate_names") val alternate_names: List<String?>?,
+    @Keep @SerializedName("ancestry") val ancestry: String?,
+    @Keep @SerializedName("dateOfBirth") val dateOfBirth: String?,
+    @Keep @SerializedName("eyeColour") val eyeColour: String?,
+    @Keep @SerializedName("gender") val gender: String?,
+    @Keep @SerializedName("hairColour") val hairColour: String?,
+    @Keep @SerializedName("hogwartsStaff") val hogwartsStaff: Boolean?,
+    @Keep @SerializedName("hogwartsStudent") val hogwartsStudent: Boolean?,
+    @Keep @SerializedName("house") val house: String?,
+    @Keep @SerializedName("id") val id: String?,
+    @Keep @SerializedName("image") val image: String?,
+    @Keep @SerializedName("name") val name: String?,
+    @Keep @SerializedName("patronus") val patronus: String?,
+    @Keep @SerializedName("species") val species: String?,
+    @Keep @SerializedName("wand") val wand: Wand?,
+    @Keep @SerializedName("wizard") val wizard: Boolean?,
+    @Keep @SerializedName("yearOfBirth") val yearOfBirth: Int?
 )
